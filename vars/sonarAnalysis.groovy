e5aaa892c622ed24611a43fff530ede6279aeed8
Iván López Ávila <149825835+ivanloav@users.noreply.github.com>
2024-09-24 20:18:33 +0200

    Actualizar función de análisis de Sonar para ejecutar pruebas de calidad de código en Jenkins

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,17 +1,26 @@
 def call(Map params = [:]) {
     boolean abortPipeline = params.get('abortPipeline', false)
     boolean qualityGateCheck = params.get('qualityGateCheck', false)
+    def branchName = env.BRANCH_NAME
 
     withSonarQubeEnv('Sonar Local') {
         sh 'echo "Ejecución de las pruebas de calidad de código"'
         
         timeout(time: 5, unit: 'MINUTES') {
             echo 'Esperando a los resultados del análisis de SonarQube...'
+            
             if (qualityGateCheck) {
                 def qualityGateStatus = 'PASSED'
                 echo "QualityGate result: ${qualityGateStatus}"
-                if (qualityGateStatus == 'FAILED' && abortPipeline) {
+
+                if (abortPipeline) {
                     error 'El análisis de calidad ha fallado y se ha abortado el pipeline.'
+                } else {
+                    if (branchName == 'master' || branchName.startsWith('hotfix')) {
+                        if (qualityGateStatus == 'FAILED') {
+                            error "El análisis de calidad ha fallado en la rama '${branchName}' y se ha abortado el pipeline."
+                        }
+                    }
                 }
             }
         }
